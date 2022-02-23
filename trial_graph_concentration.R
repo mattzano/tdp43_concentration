6eab0d6f9c99953c0c8c6be5edc3dc7b6f40cc74
mattzano <superdino00@gmail.com>
2022-02-23 11:36:43 +0000

    committment

--- trial_graph_concentration.R
+++ trial_graph_concentration.R
@@ -1,19 +1,3 @@
-#shared_cryptic = tbl %>%
-#  mutate(name = glue::glue("{gene_name}|{junc_cat}|{paste_into_igv_junction}")) %>%
-#  dplyr::select(name,is_a_cryptic,comparison) %>%
-#  unique() %>%
-#  filter(is_a_cryptic == T) %>% 
-#  mutate(event_present = is_a_cryptic) %>%
-#  pivot_wider(values_from = "event_present",
-#              id_cols = "name",
-#              names_from = "comparison",
-#              values_fill = FALSE,values_fn = sum) %>%
-#  column_to_rownames('name') %>%
-#  mutate_all(as.logical)
-#upset(big_data, nsets = 5, number.angles = 30, point.size = 3.5, line.size = 2)
-
-
-
 #####import dpsi table and filter for relevant events
 base <- "noDox"
 contrast_input <- c("dox00125", 
@@ -35,8 +19,8 @@ for (i in contrast_input) {
 big_delta <- rbindlist(datalista, idcol = TRUE)
 
 big_delta_filter <- big_delta %>%
-  filter(mean_dpsi_per_lsv_junction > 0 &
-           probability_changing > 0.9)
+  filter(no_dox_mean_psi < 0.05 & contrast_mean_psi >= 0.1)
+  #filter(mean_dpsi_per_lsv_junction > 0 & probability_changing > 0.9)
 
 ######import parsed data, merge them, normalize data from 0 (ctrl) to 1 (0.075) !!!
 input_list <- c("no_dox",
@@ -62,61 +46,133 @@ big_data$.id <- factor(big_data$.id, levels = c("no_dox",
                                                 "dox_025", 
                                                 "dox_075"))
 
-big_data_filtered <- big_data %>%
+#big_data_filter <- big_delta %>%
+#  group_by(lsv_junc) %>%
+#  filter(n() ==  5) %>%
+#  mutate(mean_psi_per_lsv_junction_normal = (contrast_mean_psi - no_dox_mean_psi)
+#         / (contrast_mean_psi[.id == "dox0075"] - no_dox_mean_psi)) #%>%
+#  filter(mean_psi_per_lsv_junction_normal[.id == "dox0075"] >= mean_psi_per_lsv_junction_normal[.id == "dox0025"] &
+#         mean_psi_per_lsv_junction_normal[.id == "dox0025"] >= mean_psi_per_lsv_junction_normal[.id == "dox0021"] &
+#         mean_psi_per_lsv_junction_normal[.id == "dox0021"] >= mean_psi_per_lsv_junction_normal[.id == "dox00187"] &
+#         mean_psi_per_lsv_junction_normal[.id == "dox00187"] >= mean_psi_per_lsv_junction_normal[.id == "dox00125"]) %>%
+#  filter(mean_psi_per_lsv_junction_normal >= 0)# %>%
+#  filter(mean_psi_per_lsv_junction_normal[.id == "dox0025"] < 0.3)
+
+ 
+big_data_filtereds <- big_data %>%
   filter(lsv_junc %in% big_delta_filter$lsv_junc) %>% #try without cryptics
   group_by(lsv_junc) %>%
   filter(n() == 6) %>%
+  filter(mean_psi_per_lsv_junction[.id == "no_dox"] < 0.05) %>%
+  filter(mean_psi_per_lsv_junction[.id == "dox_025"]  > 0.01) %>%
+  
+  filter((mean_psi_per_lsv_junction[.id == "dox_075"] - mean_psi_per_lsv_junction[.id == "dox_025"] > -0.1) &
+         (mean_psi_per_lsv_junction[.id == "dox_025"] - mean_psi_per_lsv_junction[.id == "dox_021"] > -0.1) &
+         (mean_psi_per_lsv_junction[.id == "dox_021"] - mean_psi_per_lsv_junction[.id == "dox_0187"] > -0.1) &
+         (mean_psi_per_lsv_junction[.id == "dox_0187"] - mean_psi_per_lsv_junction[.id == "dox_0125"] > -0.1) &
+         (mean_psi_per_lsv_junction[.id == "dox_0125"] - mean_psi_per_lsv_junction[.id == "no_dox"] > -0.1)) %>%
+  
   mutate(mean_psi_per_lsv_junction_normal = (mean_psi_per_lsv_junction - mean_psi_per_lsv_junction[.id == "no_dox"])
-         / (mean_psi_per_lsv_junction[.id == "dox_075"] - mean_psi_per_lsv_junction[.id == "no_dox"]))
-
-big_data_filtere <- big_data_filtered %>%
-  group_by(lsv_junc) %>%
-  filter(n() == 6) %>%
-  filter(mean_psi_per_lsv_junction_normal[.id == "dox_075"] >= mean_psi_per_lsv_junction_normal[.id == "dox_025"] &
-           mean_psi_per_lsv_junction_normal[.id == "dox_025"] >= mean_psi_per_lsv_junction_normal[.id == "dox_021"] &
-           mean_psi_per_lsv_junction_normal[.id == "dox_021"] >= mean_psi_per_lsv_junction_normal[.id == "dox_0187"] &
-           mean_psi_per_lsv_junction_normal[.id == "dox_0187"] >= mean_psi_per_lsv_junction_normal[.id == "dox_0125"]) %>%
-  filter(mean_psi_per_lsv_junction_normal >= 0) 
-
-big_data_filter <- big_data_filtere %>%
-  group_by(lsv_junc) %>%
-  filter(n() == 6) %>%
-  filter(mean_psi_per_lsv_junction_normal[.id == "dox_025"] < 0.3 &
-           mean_psi_per_lsv_junction_normal[.id == "dox_025"] > 0)
-
-big_data_filtere %>%
-  #filter(gene_name == "INSR") %>% 
-  ggplot(mapping = aes(x = .id, y = mean_psi_per_lsv_junction_normal, group = lsv_junc, color = gene_name)) +
-    geom_line() +
-    theme_classic()
-
-
-
-
-
-big_data_matrix <- big_data %>%
-  filter(lsv_junc %in% big_delta_filter$lsv_junc) %>%
-  mutate(no_dox = ifelse(.id %in% "no_dox", 1, 0),
-         dox_0125 = ifelse(.id %in% "dox_0125", 1, 0),
-         dox_0187 = ifelse(.id %in% "dox_0187", 1, 0),
-         dox_021 = ifelse(.id %in% "dox_021", 1, 0),
-         dox_025 = ifelse(.id %in% "dox_025", 1, 0),
-         dox_075 = ifelse(.id %in% "dox_075", 1, 0))
-
-big_data_matrix_2 <- big_data_matrix[,c(2,12,19:24)] %>%
-  group_by(gene_name,lsv_junc) %>%
-  summarise_at(vars(contains('dox')), ~ max(as.numeric(.), na.rm = TRUE))
-
-big_data_matrix_2[,3:8] <- lapply(big_data_matrix_2[,3:8], as.integer)
-
-big_data_matrix_3 <- as.data.frame(big_data_matrix_2)
-
-upset(big_data_matrix_3, nsets = 6, sets = c("no_dox",
-                                             "dox_0125", 
-                                             "dox_0187", 
-                                             "dox_021", 
-                                             "dox_025", 
-                                             "dox_075"), order.by = "freq", keep.order = TRUE,
-      mainbar.y.label = "Junction Intersections", sets.x.label = "Junction Per Level of TDP-43 KD")
+         / (mean_psi_per_lsv_junction[.id == "dox_075"] - mean_psi_per_lsv_junction[.id == "no_dox"])) %>%
+  
+  filter((mean_psi_per_lsv_junction_normal[.id == "dox_075"] - mean_psi_per_lsv_junction_normal[.id == "dox_025"] > -0.1) &
+      (mean_psi_per_lsv_junction_normal[.id == "dox_025"] - mean_psi_per_lsv_junction_normal[.id == "dox_021"] > -0.1) &
+      (mean_psi_per_lsv_junction_normal[.id == "dox_021"] - mean_psi_per_lsv_junction_normal[.id == "dox_0187"] > -0.1) &
+      (mean_psi_per_lsv_junction_normal[.id == "dox_0187"] - mean_psi_per_lsv_junction_normal[.id == "dox_0125"] > -0.1) &
+      (mean_psi_per_lsv_junction_normal[.id == "dox_0125"] - mean_psi_per_lsv_junction_normal[.id == "no_dox"] > -0.1)) %>%
+  
+  mutate(color_gene_name = as.factor(as.character(ifelse(paste_into_igv_junction %in% c("chr19:17641556-17642414",
+                                                                              "chr8:79611214-79616822",
+                                                                              "chr20:63439708-63444659",
+                                                                              "chr10:3099819-3101365",
+                                                                              "chr2:241668985-241670726",
+                                                                              "chr9:128956174-128956350"), 1,
+                                        #ifelse(mean_psi_per_lsv_junction_normal[.id == "dox_0125"] > 0.2, 0,
+                                        #ifelse(mean_psi_per_lsv_junction_normal[.id == "dox_0187"] > 0.2, 0,
+                                        #ifelse(mean_psi_per_lsv_junction_normal[.id == "dox_021"] > 0.2, 0.5,
+                                        ifelse((mean_psi_per_lsv_junction_normal[.id == "dox_0125"] < 0.1 &
+                                                 mean_psi_per_lsv_junction_normal[.id == "dox_0187"] < 0.1 &
+                                                 mean_psi_per_lsv_junction_normal[.id == "dox_021"] < 0.1 &
+                                                 mean_psi_per_lsv_junction_normal[.id == "dox_025"] < 0.1), 0.5, 0)))), 
+         levels = c(0,0,0.5,0.5,1,1)) %>%
+  
+  mutate(label_junction = case_when((paste_into_igv_junction %in% c("chr19:17641556-17642414",
+                                                                    "chr8:79611214-79616822",
+                                                                    "chr20:63439708-63444659",
+                                                                    "chr10:3099819-3101365",
+                                                                    "chr2:241668985-241670726",
+                                                                    "chr9:128956174-128956350") &
+                                       .id =="dox_0187") |
+                                    (mean_psi_per_lsv_junction_normal[.id == "dox_025"] < 0.1 & .id == "dox_025") 
+                                    ~ gene_name,
+                                    T ~ ""))
 
 
+big_data_filtereds %>%
+  #filter(gene_name == "STMN2") %>%
+  ggplot(mapping = aes(x = .id, y = mean_psi_per_lsv_junction, group = lsv_junc)) +
+  geom_line(aes(color = color_gene_name, alpha = color_gene_name), show.legend = F) +
+  geom_text_repel(aes(x = .id, y = mean_psi_per_lsv_junction, label = label_junction), point.padding = 0.3,
+                  nudge_y = 0.2, min.segment.length = 0.5, box.padding  = 2, max.overlaps = Inf, size=4, show.legend = F) +
+  scale_color_manual(values = c("gray", "#D53E4F", "#3288BD")) +
+  scale_alpha_manual(values = c(0.2,1,1)) +
+  
+  theme_classic()
+
+#shared_cryptic = tbl %>%
+#  mutate(name = glue::glue("{gene_name}|{junc_cat}|{paste_into_igv_junction}")) %>%
+#  dplyr::select(name,is_a_cryptic,comparison) %>%
+#  unique() %>%
+#  filter(is_a_cryptic == T) %>% 
+#  mutate(event_present = is_a_cryptic) %>%
+#  pivot_wider(values_from = "event_present",
+#              id_cols = "name",
+#              names_from = "comparison",
+#              values_fill = FALSE,values_fn = sum) %>%
+#  column_to_rownames('name') %>%
+#  mutate_all(as.logical)
+
+big_data_matrix = big_delta %>%
+  filter(junc_cat != "annotated") %>%
+  mutate(is_a_cryptic = no_dox_mean_psi < 0.05 & contrast_mean_psi >= 0.1) %>%
+           #abs(mean_dpsi_per_lsv_junction) > 0.15 & probability_changing > 0.9) %>% # 
+  mutate(name = glue::glue("{gene_name}|{junc_cat}|{paste_into_igv_junction}")) %>%
+  dplyr::select(name,is_a_cryptic,.id) %>%
+  unique() %>%
+  filter(is_a_cryptic == T)%>% 
+  pivot_wider(values_from = "is_a_cryptic",
+              id_cols = "name",
+              names_from = ".id",
+              values_fill = FALSE, values_fn = sum) %>%
+  column_to_rownames('name') %>%
+  mutate_all(as.logical)
+  
+upset(big_data_matrix * 1,
+      order.by = "freq", keep.order = TRUE,
+      mainbar.y.label = "Junction Intersections", 
+      sets.x.label = "Junction Per Level of TDP-43 KD")
+
+#big_data_matrix <- big_data %>%
+#  filter(lsv_junc %in% big_delta_filter$lsv_junc) %>%
+#  mutate(no_dox = ifelse(.id %in% "no_dox", 1, 0),
+#         dox_0125 = ifelse(.id %in% "dox_0125", 1, 0),
+#         dox_0187 = ifelse(.id %in% "dox_0187", 1, 0),
+#         dox_021 = ifelse(.id %in% "dox_021", 1, 0),
+#         dox_025 = ifelse(.id %in% "dox_025", 1, 0),
+#         dox_075 = ifelse(.id %in% "dox_075", 1, 0))
+
+#big_data_matrix_2 <- big_data_matrix[,c(2,12,19:24)] %>%
+#  group_by(gene_name,lsv_junc) %>%
+#  summarise_at(vars(contains('dox')), ~ max(as.numeric(.), na.rm = TRUE))
+
+#big_data_matrix_2[,3:8] <- lapply(big_data_matrix_2[,3:8], as.integer)
+
+#big_data_matrix_3 <- as.data.frame(big_data_matrix_2)
+
+#upset(big_data_matrix_3, nsets = 6, sets = c("no_dox",
+#                                             "dox_0125", 
+#                                             "dox_0187", 
+#                                             "dox_021", 
+#                                             "dox_025", 
+#                                             "dox_075"), order.by = "freq", keep.order = TRUE,
+#      mainbar.y.label = "Junction Intersections", sets.x.label = "Junction Per Level of TDP-43 KD")
